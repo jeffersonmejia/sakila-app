06ee3ea26b88855a7b775c131e38f786d48d66ab
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2026-04-25 12:51:59 -0500

    feat: change value pool size

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,63 +3,41 @@ const d = document,
 	$table = d.getElementById('filmsTable'),
 	$inputId = d.getElementById('filmId'),
 	$inputTitle = d.getElementById('title'),
-	$inputLanguage = d.getElementById('languageId')
+	$inputLanguage = d.getElementById('languageId'),
+	$btnPrev = d.getElementById('btnPrev'),
+	$btnNext = d.getElementById('btnNext')
 
-d.addEventListener('click', (e) => {
-	if (e.target.id === 'btnLoad') return handleLoad()
-
-	if (e.target.id === 'btnCreate') return handleCreate()
-
-	if (e.target.id === 'btnUpdate') return handleUpdate()
-
-	if (e.target.matches('.edit')) return handleEdit(e.target)
-
-	if (e.target.matches('.delete')) return handleDelete(e.target)
-})
-d.addEventListener('DOMContentLoaded', init)
-
-function handleLoad() {
-	loadFilms()
-}
-
-function handleCreate() {
-	createFilm()
-}
-
-function handleUpdate() {
-	updateFilm()
-}
-
-function handleEdit($el) {
-	const id = $el.dataset.id
-	const title = $el.dataset.title
-	const lang = $el.dataset.lang
-
-	selectFilm({ filmId: id, title, languageId: lang })
-}
-
-function handleDelete($el) {
-	deleteFilm($el.dataset.id)
-}
+let currentPage = 0
+const pageSize = 8
 
 async function loadFilms() {
-	const res = await fetch(API)
-	const data = await res.json()
+	try {
+		const res = await fetch(`${API}?page=${currentPage}&size=${pageSize}`)
+		let data = await res.json()
 
-	$table.innerHTML = ''
+		$table.innerHTML = ''
 
-	data.forEach((f) => {
-		$table.innerHTML += `
-            <tr>
-                <td>${f.filmId}</td>
-                <td>${f.title}</td>
-                <td>
-                    <button class="edit" data-id="${f.filmId}" data-title="${f.title}" data-lang="${f.languageId}">Edit</button>
-                    <button class="delete" data-id="${f.filmId}">Delete</button>
-                </td>
-            </tr>
-        `
-	})
+		if (Array.isArray(data)) {
+			data = data.slice(currentPage * pageSize, (currentPage + 1) * pageSize)
+		} else {
+			data = data.content
+		}
+
+		data.forEach((f) => {
+			$table.innerHTML += `
+				<tr>
+					<td>${f.filmId}</td>
+					<td>${f.title}</td>
+					<td>
+						<button class="edit" data-id="${f.filmId}" data-title="${f.title}" data-lang="${f.languageId}">Edit</button>
+						<button class="delete" data-id="${f.filmId}">Delete</button>
+					</td>
+				</tr>
+			`
+		})
+	} catch (e) {
+		console.error(e)
+	}
 }
 
 function selectFilm(f) {
@@ -80,7 +58,6 @@ async function createFilm() {
 			replacementCost: 19.99,
 		}),
 	})
-
 	resetForm()
 	loadFilms()
 }
@@ -100,7 +77,6 @@ async function updateFilm() {
 			replacementCost: 19.99,
 		}),
 	})
-
 	resetForm()
 	loadFilms()
 }
@@ -116,6 +92,51 @@ function resetForm() {
 	$inputLanguage.value = ''
 }
 
+function handleLoad() {
+	loadFilms()
+}
+
+function handleCreate() {
+	createFilm()
+}
+
+function handleUpdate() {
+	updateFilm()
+}
+
+function handleEdit($el) {
+	const id = $el.dataset.id
+	const title = $el.dataset.title
+	const lang = $el.dataset.lang
+	selectFilm({ filmId: id, title, languageId: lang })
+}
+
+function handleDelete($el) {
+	deleteFilm($el.dataset.id)
+}
+
+function handlePrev() {
+	if (currentPage > 0) currentPage--
+	loadFilms()
+}
+
+function handleNext() {
+	currentPage++
+	loadFilms()
+}
+
 function init() {
 	loadFilms()
 }
+
+d.addEventListener('click', (e) => {
+	if (e.target.id === 'btnLoad') return handleLoad()
+	if (e.target.id === 'btnCreate') return handleCreate()
+	if (e.target.id === 'btnUpdate') return handleUpdate()
+	if (e.target.id === 'btnPrev') return handlePrev()
+	if (e.target.id === 'btnNext') return handleNext()
+	if (e.target.matches('.edit')) return handleEdit(e.target)
+	if (e.target.matches('.delete')) return handleDelete(e.target)
+})
+
+d.addEventListener('DOMContentLoaded', init)
