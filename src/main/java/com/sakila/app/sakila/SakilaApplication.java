01b8047c6eeb9b3a58044f0f7933c796ed8fa7ae
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2026-05-02 09:36:18 -0500

    feat: add view to pool

--- src/main/java/com/sakila/app/sakila/SakilaApplication.java
+++ src/main/java/com/sakila/app/sakila/SakilaApplication.java
@@ -1,13 +1,36 @@
 package com.sakila.app.sakila;
 
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariPoolMXBean;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SakilaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SakilaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SakilaApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner monitorPool(DataSource dataSource) {
+        return args -> {
+            HikariDataSource hikari = (HikariDataSource) dataSource;
+            HikariPoolMXBean pool = hikari.getHikariPoolMXBean();
+
+            while (true) {
+                System.out.println("Total: " + pool.getTotalConnections());
+                System.out.println("Activas: " + pool.getActiveConnections());
+                System.out.println("Idle: " + pool.getIdleConnections());
+                System.out.println("Esperando: " + pool.getThreadsAwaitingConnection());
+                System.out.println("-----");
+
+                Thread.sleep(3000);
+            }
+        };
+    }
 }
